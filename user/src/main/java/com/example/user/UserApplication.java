cf6edfbd0c7e88876dc43979e3f10ec6fa5de9a8
George <wjq008wang@163.com>
2023-05-20 16:21:16 +0800

    commit the files

--- user/src/main/java/com/example/user/UserApplication.java
+++ user/src/main/java/com/example/user/UserApplication.java
@@ -1,11 +1,17 @@
 package com.example.user;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@MapperScan("com.example.user.mapper")
+@EnableSwagger2
+@EnableScheduling
 public class UserApplication {
 
 	public static void main(String[] args) {
--- user/src/main/java/com/example/user/controller/OrderController.java
+++ user/src/main/java/com/example/user/controller/OrderController.java
@@ -5,22 +5,40 @@ import com.example.user.model.Order;
 import com.example.user.service.OrderService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Callable;
 
 @Api(tags = "股票信息")
 @RestController
+@Slf4j
 @RequestMapping("/order")
 public class OrderController {
     @Autowired
     private OrderService orderService;
     @ApiOperation("添加银行信息")
     @PostMapping("/add")
-    public Result<String> add(@RequestBody Order order) {
-        return orderService.add(order);
+    public    Callable<String>  add(@RequestBody Order order) {
+        log.info("主线程开始");
+        //使用Runnable异步处理Rest服务
+        //开启一个线程,方便以后多个用户下单时出现等待现象。
+        Callable<String> result = new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                // TODO Auto-generated method stub
+                log.info("副线程开始");
+                Thread.sleep(1000);
+                log.info("副线程结束");
+                orderService.add(order);
+                return "success";
+            }
+        };
+        log.info("主线程结束");
+        return result;
     }
 
     @ApiOperation("修改银行信息")
